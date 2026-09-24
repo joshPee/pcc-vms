c27bbf3b963c547b1160c3d6b2f39cf13103053d
joshPee <joshPee@users.noreply.github.com>
2026-09-24 10:38:36 +0000

    Move check-in button up by removing all margins
    - Removed progress indicator margin (mb-0)
    - Reduced carousel bottom padding from pb-3 to pb-2
    - This moves the button as close as possible to the carousel elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
       <div className="flex-1 overflow-hidden">
         <div
           ref={carouselRef}
-          className="flex overflow-x-auto snap-x snap-mandatory gap-3 px-4 pb-3"
+          className="flex overflow-x-auto snap-x snap-mandatory gap-3 px-4 pb-2"
           style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}
         >
           {CAROUSEL_SLIDES.map((slide, index) => (
@@ -92,7 +92,7 @@ export default function Home() {
         </div>
 
         {/* Progress Indicator */}
-        <div className="flex justify-center gap-[6px] mb-1">
+        <div className="flex justify-center gap-[6px] mb-0">
           {CAROUSEL_SLIDES.map((_, index) => (
             <div
               key={index}
